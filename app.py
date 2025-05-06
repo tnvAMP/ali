7ce9d2de85a346aac6dbf0c885b79ee0ac396def
diyorbek <diyorbektursunbekoc@gmail.com>
2025-05-06 16:01:46 +0500

    calculate_painting_time degan funksiya yozildi

---
+++ app.py
@@ -0,0 +1,13 @@
+def calculate_painting_time (colors: list) -> int:
+    result = 2
+    if colors == []:
+        return 0
+    for i in range(len(colors) - 1):
+        if colors[i] == colors[i + 1]:
+            result += 2
+        else:
+            result += 2 + 1
+    return result
+colors = []
+result = 2
+print(calculate_painting_time(colors))
